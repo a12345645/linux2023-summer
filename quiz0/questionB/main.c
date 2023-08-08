acf7984521d4bc509436bf30fbdb68c8a07ce2d0
a12345645 <a12345645kimo.com@gmail.com>
2023-08-08 16:09:16 +0800

    questionB modify the answer

--- quiz0/questionB/main.c
+++ quiz0/questionB/main.c
@@ -1,16 +1,20 @@
 #include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
     uintptr_t mask = alignment - 1;
-    if ((alignment & mask) == 0) {  /* power of two? */
-        return sz + (~sz & mask) + 1;       
+    if ((alignment & mask) == 0) {     /* power of two? */
+        return sz + (-sz & mask);  // MMMM
     }
     return (((sz + mask) / alignment) * alignment);
 }
 
-int main () {
-    printf("%d %ld",121, align_up(121, 4));
+int main()
+{
+    printf("%d %ld\n", 123, align_up(123, 4));
+    printf("%d %ld\n", 122, align_up(122, 4));
+    printf("%d %ld\n", 121, align_up(121, 4));
+    printf("%d %ld\n", 120, align_up(120, 4));
 }
